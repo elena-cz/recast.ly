cfff3bedc783a21e5e73a87c4091f58ead7bd20f
elena-cz <elenaczubiak@gmail.com>
2017-10-26 20:11:06 -0700

    Add autoplay toggle

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,23 +1,58 @@
-var VideoPlayer = (props) => {
-  
-  if (props.video.id) {
-    return (
-      <div className="video-player">
-        <div className="embed-responsive embed-responsive-16by9">
-          <iframe className="embed-responsive-item" src={'https://www.youtube.com/embed/' + props.video.id.videoId + '?autoplay=1'} allowFullScreen></iframe>
-        </div>
-        <div className="video-player-details">
-          <h3>{props.video.snippet.title}</h3>
-          <div>{props.video.snippet.description}</div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="video-player"><p>Loading...</p></div>
-    );
+class VideoPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      autoplay: true
+    };
+    this.updateAutoplay = this.updateAutoplay.bind(this);
+    this.buildVideoURL = this.buildVideoURL.bind(this);
   }
-};
+  updateAutoplay() {
+    console.log('ive been updated'),
+    this.setState({
+      autoplay: !this.state.autoplay
+    });
+  }
+  
+  buildVideoURL() {
+    
+    var url = 'https://www.youtube.com/embed/' + this.props.video.id.videoId;
+    
+    if (this.state.autoplay) {
+      url = url + '?autoplay=1';
+    }
+    
+    return url;
+    
+  }  
+
+  render() {
+    if (this.props.video.id) {
+      return (
+        <div className="video-player">
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" src={this.buildVideoURL()} allowFullScreen></iframe>
+          </div>
+          <div className="video-player-details">
+            <h3>{this.props.video.snippet.title}</h3>
+            <div>{this.props.video.snippet.description}</div>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" onChange={this.updateAutoplay} /> 
+            <span className="form-check-label">
+              Autoplay
+            </span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="video-player"><p>Loading...</p></div>
+      );
+    }
+  }
+  
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
